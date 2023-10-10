0d00e1f5526311d0d8a548ff5ab300fcd65bc44c
Rastamafugg <jasondeanross@gmail.com>
2023-10-10 09:29:25 -0300

    - added chat api endpoint

---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,37 @@
+import type { Config } from '@sveltejs/kit';
+import { StreamingTextResponse, LangChainStream } from 'ai';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { AIMessage, HumanMessage } from 'langchain/schema';
+import { OPENAI_API_KEY } from "$env/static/private";
+
+export const config: Config = {
+    runtime: 'edge'
+};
+
+/**
+ * server endpoint for chatGpt Stream Chat
+ * @param request - request object
+ */
+export const POST = async ({ request }) => {
+    const { messages } = await request.json();
+    const { stream, handlers } = LangChainStream();
+
+    const llm = new ChatOpenAI({
+        streaming: true,
+        openAIApiKey: OPENAI_API_KEY
+    });
+
+    llm
+        .call(
+            messages.map((m) =>
+                m.role == 'user' ?
+                    new HumanMessage(m.content)
+                    : new AIMessage(m.content)
+            ),
+            {},
+            [handlers]
+        )
+        .catch(console.error);
+
+    return new StreamingTextResponse(stream);
+};
