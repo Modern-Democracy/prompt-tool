408368126f13f4a929717fea74ad779f2a3e6509
Rastamafugg <jasondeanross@gmail.com>
2023-10-16 15:57:09 -0300

    - retrieved the user's uid after validating the auth token passed in by the request. - also corrected the chain call function to work with the example code this endpoint is based off of

--- src/routes/api/memory-chat/+server.ts
+++ src/routes/api/memory-chat/+server.ts
@@ -1,22 +1,33 @@
 import type { Config } from '@sveltejs/kit';
 import { StreamingTextResponse, LangChainStream } from 'ai';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import {AIMessage, FunctionMessage, HumanMessage, SystemMessage} from 'langchain/schema';
 import {BufferMemory} from "langchain/memory";
 import {ConversationChain} from "langchain/chains";
-import {FirestoreChatMessageHistory} from "langchain/dist/stores/message/firestore";
+import {FirestoreChatMessageHistory} from "langchain/stores/message/firestore";
 import {FIREBASE_ADMIN_KEY, OPENAI_API_KEY} from "$env/static/private";
 import admin from "firebase-admin"
+import {getFirebaseAdminAuth} from "../../../lib/firebase/firebase-admin";
 export const config: Config = {
     runtime: 'edge'
 };
+
+async function getUserId(authHeader: string) {
+    const tokenString = authHeader.substring(7);
+    const token = await getFirebaseAdminAuth().verifyIdToken(tokenString)
+    return (token) ? token.uid : 'test-user';
+}
 
 /**
  * server endpoint for chatGpt Stream Chat
  * @param request - request object
  */
 export const POST = async ({ request }) => {
-    const { messages, sessionId = 'test-session', userId = 'test-user' } = await request.json();
+    const { messages, ...other } = await request.json();
+    const authHeader = request.headers.get('authorization');
+    const userId = (authHeader) ? await getUserId(authHeader) : 'test-user'; //Remove Bearer prefix if set, otherwise default to test-user
+    const { sessionId = 'test-session'  } = other;
+
+    console.log('SESSION:', sessionId, 'USER:', userId);
     const { stream, handlers } = LangChainStream();
 
     const memory = new BufferMemory({
@@ -32,21 +43,10 @@ export const POST = async ({ request }) => {
         streaming: true,
         openAIApiKey: OPENAI_API_KEY
     });
-    const chain = new ConversationChain({ llm, memory: new BufferMemory() });
+    const chain = new ConversationChain({ llm, memory });
 
     await chain
-        .call(
-            messages.map((m) => {
-                switch(m.role) {
-                    case 'system': return new SystemMessage(m.content);
-                    case 'user': return new HumanMessage(m.content);
-                    case 'function': return new FunctionMessage(m.content);
-                    default: return new AIMessage(m.content);
-                }
-            }
-            ),
-            {},
-        )
+        .call({ input: messages[messages.length - 1].content })
         .catch(console.error);
 
     return new StreamingTextResponse(stream);
