b96af28abfe4b9e8a9afc489cf35e75dfbb7c627
Rastamafugg <jasondeanross@gmail.com>
2023-10-21 17:33:09 -0300

    - added simple page HTML and CSS structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,5 +25,94 @@
 	let token = null;
 	let isLoggedIn = false;
 </script>
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 
-<slot />
+<style>
+    body, html {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        font-family: Arial, sans-serif; /* Open font similar to the example */
+    }
+
+    .container {
+        display: flex;
+        height: 100vh;
+    }
+
+    .left-panel {
+        background-color: #000;
+        width: 20%;
+        color: #FFF;
+    }
+
+    .menu-toggle {
+        text-align: right;
+        padding: 1em;
+    }
+
+    .chat-list {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .chat-list li {
+        padding: 1em;
+        cursor: pointer;
+    }
+
+    .chat-list li.active, .chat-list li:hover {
+        background-color: #333;
+    }
+
+    .chat-list li:hover .hover-icons, .chat-list li.active .hover-icons {
+        display: inline;
+    }
+
+    .hover-icons {
+        float: right;
+    }
+
+    .user-panel {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        background-color: #111;
+        color: #FFF;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1em;
+    }
+
+    input[type="text"] {
+        flex-grow: 1;
+        border: 1px solid #CCC;
+    }
+</style>
+
+<div class="container">
+    <!-- Left Menu Panel -->
+    <div class="left-panel">
+        <div class="menu-toggle">
+            <span class="material-symbols-outlined">person</span>
+        </div>
+        <ul class="chat-list">
+            <li class="active">
+                <span>Conversation 1</span>
+                <div class="hover-icons">
+                    <span class="material-symbols-outlined">edit</span>
+                    <span class="material-symbols-outlined">delete</span>
+                </div>
+            </li>
+            <li><span>Conversation 2</span></li>
+        </ul>
+        <div class="user-panel">
+            <span class="material-symbols-outlined">person</span>
+            <span>Username</span>
+            <span class="material-symbols-outlined">person</span>
+        </div>
+    </div>
+    <slot/>
+</div>
